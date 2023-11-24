1c2da84fe67176feae95a6e4f332a6fea7059782
DragonOnPi <dhruva.vms@icloud.com>
2023-11-24 13:06:25 -0700

    Initiall Commit

---
+++ calculator.py
@@ -0,0 +1,12 @@
+x = float(input("What's x?" ))
+
+y = float(input("What's y?" ))
+
+
+print("Dividing...")
+
+
+z = float(x) / float(y)
+
+
+print(f"{z:,}")
---
+++ hello.py
@@ -0,0 +1,14 @@
+
+
+
+def main():
+    name = input("What's your name?")
+    hello(name)
+def hello(to="there"):
+    print("Hello", to)
+
+
+def hello(to="world"):
+    print("Hello there",to)
+
+main()
